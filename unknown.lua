a69e94d34a7adcfd9e637ae229f41b53de5abcf8
KongkiatDev <132956634+KongkiatDev@users.noreply.github.com>
2023-06-29 03:33:01 +0700

    Update unknown.lua

--- unknown.lua
+++ unknown.lua
@@ -6,6 +6,7 @@
 
 --#region Get Service
 repeat task.wait() until game:IsLoaded()
+repeat task.wait() until game.Workspace:FindFirstChild(game.Players.LocalPlayer.Name)
 game.CoreGui.RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(e)
   if e.Name == 'ErrorPrompt' then
     game:GetService("TeleportService"):Teleport(8304191830, game.Players.LocalPlayer)
@@ -28,15 +29,11 @@ local ReplicatedFirst = game:GetService("ReplicatedFirst")
 local TweenService = game:GetService("TweenService")
 local StarterGui = game:GetService("StarterGui")
 local VirtualUser = game:GetService("VirtualUser")
-local Services = require(game:GetService("ReplicatedStorage").src.Loader)
-local Request = http_request or (syn and syn.request)
 
 if game.PlaceId == ANIME_ADVENTURES_ID then
-  repeat task.wait() until game.Workspace:FindFirstChild(game.Players.LocalPlayer.Name)
   repeat task.wait() until game.Players.LocalPlayer.PlayerGui:FindFirstChild("collection"):FindFirstChild("grid"):FindFirstChild("List"):FindFirstChild("Outer"):FindFirstChild("UnitFrames")
   repeat task.wait() until LocalPlayer.PlayerGui.BattlePass.Main.Level.V.Text ~= "99"
 else
-  repeat task.wait() until game.Workspace:FindFirstChild(game.Players.LocalPlayer.Name)
   game:GetService("ReplicatedStorage").endpoints.client_to_server.vote_start:InvokeServer()
   repeat task.wait() until game:GetService("Workspace")["_waves_started"].Value == true
   LocalPlayer.PlayerGui.MessageGui.Enabled = false
@@ -44,6 +41,9 @@ else
   game:GetService("ReplicatedStorage").packages.assets["ui_sfx"].error_old.Volume = 0
   _G.start_time = os.time()
 end
+
+local Services = require(game:GetService("ReplicatedStorage").src.Loader)
+local Request = http_request or (syn and syn.request)
 
 --#endregion
 
